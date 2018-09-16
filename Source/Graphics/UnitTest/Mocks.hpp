659f7c2be65bbe203a84f1ec3d6b1458c84a079e
travisko <marek.cern@gmail.com>
2018-09-16 18:46:08 +0200

    Adding gtest and gmock third party libraries. Setting up example unit test for Graphics module.

---
+++ Source/Graphics/UnitTest/Mocks.hpp
@@ -0,0 +1,19 @@
+/*
+* @file     Mocks.hpp
+* @author   Marek Cernak
+* @date     9/16/2018
+*
+* @brief Graphics module mocks
+*/
+
+#include "gmock/gmock.h"
+
+#pragma once
+
+namespace gdt
+{
+    namespace test
+    {
+        /*Put mocks under*/
+    }
+}
