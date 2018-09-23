f3c591513e90ab64310f5bc1cdbf571b7eb528d4
ludvigarlebrink <ludvig.arlebrink@outlook.com>
2018-09-23 19:42:09 +0200

    Auto documentation shellscript.

--- Documentation/GenerateTechnicalDocumentation.sh
+++ Documentation/GenerateTechnicalDocumentation.sh
@@ -3,11 +3,12 @@ git clone https://github.com/GDTEngine/gdtengine.github.io
 echo "Removing current doxygen documentation..."
 rm -rf gdtengine.github.io/cppdoc
 
-// echo 'Generating doxygen code documentation...'
-// doxygen DoxygenConfig.log
-// 
-// cd gdtengine.github.io/cppdoc
+echo 'Generating doxygen code documentation...'
+doxygen DoxygenConfig.log
+ 
+cd gdtengine.github.io
 
+git add .
 git commit -a -m "Cpp Documentation."
 git push
 
