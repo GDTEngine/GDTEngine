55fc7a89fe81a576b18aded5a213130d388a7835
ludvigarlebrink <ludvig.arlebrink@outlook.com>
2018-09-23 19:23:24 +0200

    Cpp Documentation.

--- Documentation/GenerateTechnicalDocumentation.sh
+++ Documentation/GenerateTechnicalDocumentation.sh
@@ -1,5 +1,17 @@
-echo "Removing current doxygen documentation..."
-rm -rf cppdoc
+git clone https://github.com/GDTEngine/gdtengine.github.io
 
-echo 'Generating doxygen code documentation...'
-doxygen DoxygenConfig.log
+echo "Removing current doxygen documentation..."
+rm -rf gdtengine.github.io/cppdoc
+
+// echo 'Generating doxygen code documentation...'
+// doxygen DoxygenConfig.log
+// 
+// cd gdtengine.github.io/cppdoc
+
+git commit -a -m "Cpp Documentation."
+git push
+
+cd ..
+
+echo "Removing gdtengine.github.io..."
+rm -rf gdtengine.github.io
